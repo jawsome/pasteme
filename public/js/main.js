cf2d463419855f517176092d494bbc3f0442b9ed
Jawsome <atynefield@gmail.com>
2013-07-16 13:54:59 -0500

    Version 1.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,97 @@
 (function($) {
 
-function showPaste(data) {
- $('#pastearea').html('<pre>' + JSON.stringify(data) + '</pre>');
-
+function selectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) { //ms
+        range = doc.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { //all others
+        selection = window.getSelection();        
+        range = doc.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
 }
-
-
   var app = $.sammy(function() {
-    this.get('(/)', function() {
+    this.use('Template');
+    this.get('(/)', function(context) {
       $('#bin').show();
-      
+      $('#submitpaste').click(function() {
+        if($('#bin textarea').val() != ""){
+          $.ajax({
+            url: '/api',
+            type: 'POST',
+            dataType: 'json',
+            data: { 'paste': {'content': $('#bin textarea').val()} },
+            success: function(data) {
+              window.location = '/view/' +  data.id;
+            }
+          });
+        }; 
+      });
      
     });
 
     this.get('/view/:id', function () {
-    
+     var context = this;
       $.ajax({
         url: '/api/' + this.params['id'],
         dataType: 'json',
         beforeSend: function() {
+          console.log('Before send.');
           $('#bin').hide();
           $('#pastearea').html('Loading...');
         },
         success: function(data) {
-          showPaste(data);
+          console.log(data, 'json');
+          context.render('/templates/paste.template', {paste: data}).replace('#pastearea').then(function() {
+            $('pre').syntaxHighlight();
+            $('#newpaste').click(function() {
+              window.location = '/';
+            });
+	  });
         }
       });
     });
 
+    this.get('/fork/:id', function () {
+      var context = this;
+      $.ajax({
+        url: '/api/' + this.params['id'],
+        dataType: 'json',
+        beforeSend: function() {
+          console.log('Before send.');
+        },
+        success: function(data) {
+          console.log(data, 'json');
+            $('#bin textarea').val($('<div/>').html(data.body).text());
+            $('#pastearea').hide();
+            $('#bin').show();
+      $('#submitpaste').click(function() {
+        if($('#bin textarea').val() != ""){
+          $.ajax({
+            url: '/api',
+            type: 'POST',
+            dataType: 'json',
+            data: { 'paste': {'content': $('#bin textarea').val(), 'master': context.params['id'] } },
+            success: function(data) {
+              window.location = '/view/' +  data.id;
+            }
+          });
+        };
+      });
+     }
+    });
+   });
     this.get('(#/|/)test', function() {
     });
 
-  });
-
+});
 app.run();
 
 })(jQuery);
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var express = require('express'),
     ent = require('ent'),
     http = require('http'),
+    moment = require('moment'),
     cradle = require('cradle');
 
 var app = express(),
-    couch = new(cradle.Connection)().database('pasteme.it');
+    now = moment();
+    couch = new(cradle.Connection)().database('pastemeit');
 
+app.enable('trust proxy');
 app.configure(function() {
+  app.use(express.logger());
+  app.use(express.compress());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -22,8 +27,13 @@ app.get('/api/:id', function(req, res) {
         response = { 'error': 'Not found' };
         res.send(response);
       }
-      response = { 'id': req.params.id, 'body': doc.paste.content };
-
+      response = { 
+	'id': req.params.id, 
+	'body': doc.paste.content, 
+	'master': doc.paste.master ? doc.paste.master : null,
+	'date': doc.paste.date
+      };
+      res.send(response, 200);
     });
   }
   else {
@@ -35,16 +45,17 @@ app.get('/api/:id', function(req, res) {
 app.post('/api', function (req, res) {
   if(req.body.paste) {
     re = req.body.paste;
-    if(re.content.length() > 0) {
+    if(re.content.length > 0) {
       paste = {
         'content': ent.encode(re.content),
         'source': req.ip,
         'headers': req.headers,
-        'date': new Date().UTC()
+        'date': Date.now(),
+        'master': re.master ? re.master : null
       };
       couch.save('', { 'paste': paste }, function (err, response) {
         if (err) {
-          res.send({'error': 'Failed to save'}, 500);
+          res.send({'error': 'Failed to save', 'message': err}, 500);
         }
         res.send({'id': response.id, 'content': paste.content }, 200);
       });
